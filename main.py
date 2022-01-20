025ef9a996c62d0851c68be06d47bbdc15a04535
Anton Butkov <ant.butkov@gmail.com>
2022-01-21 01:11:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def download():
             with open("yvd.txt", 'r') as f:
                 lines = f.readlines()
                 if not lines:
-                    raise ValueError
+                    raise FileNotFoundError
                 else:
                     for line in lines:
                         links.append(line)
@@ -23,47 +23,50 @@ def download():
         for link in links:
             vid = YouTube(link)
             vid.streams.filter(file_extension='mp4').get_highest_resolution().download(output_path=os.getcwd())
-    except:
+    except Exception:
         print("                                            \u00a9 Ant.Butkov")
         print(art.text2art("YVD", "block"))
         print("         Welcome to YoutubeVideoDownloader \nTo download, enter the URL of the desired video "
               "below \n         Type 'FILE' to load URL from file \n        (Each URL must start on a new line) \n "
               "          Type 'GO' to start downloading \n          Then select the folder to save"
-            " \n      To cancel file download press 'Ctrl+C'")
-        links = []
-        while True:
-            url = input("Enter the URL: \n -> ")
-            if url.lower() == "go":
-                dst = askdirectory()
-                print('                     ---Downloading---')
-                break
-            elif url.lower() == "file":
-                try:
-                    url_file = askopenfilename()
+              " \n      To cancel file download press 'Ctrl+C'")
+        try:
+            links = []
+            while True:
+                url = input("Enter the URL: \n -> ")
+                if url.lower() == "go":
                     dst = askdirectory()
-                    print('   \u21b3 {}' .format(url_file))
-                    with open(url_file, 'r') as f:
-                        lines = f.readlines()
-                        if not lines:
-                            raise ValueError
-                        else:
-                            for line in lines:
-                                links.append(line)
                     print('                     ---Downloading---')
                     break
-                except:
-                    print('     \u21b3 Error loading URL from file ')
-            else:
-                links.append(url)
+                elif url.lower() == "file":
+                    try:
+                        url_file = askopenfilename()
+                        dst = askdirectory()
+                        print('   \u21b3 {}' .format(url_file))
+                        with open(url_file, 'r') as f:
+                            lines = f.readlines()
+                            if not lines:
+                                raise ValueError
+                            else:
+                                for line in lines:
+                                    links.append(line)
+                        print('                     ---Downloading---')
+                        break
+                    except Exception:
+                        print('     \u21b3 Error loading URL from file ')
+                else:
+                    links.append(url)
+        except Exception:
+            exit()
         for link in links:
             try:
                 vid = YouTube(link, on_progress_callback=on_progress)
                 print("\n {}.mp4".format(vid.title))
                 try:
                     vid.streams.filter(file_extension='mp4').get_highest_resolution().download(output_path=dst)
-                except:
+                except Exception:
                     print(" \n   \u21b3 Downloading error")
-            except:
+            except Exception:
                 print('\n {} \n \u21b3 Connection error' .format(link))
                 pass
 
